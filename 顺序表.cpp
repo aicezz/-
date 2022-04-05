5d81e1856a30b568518a8124dde53a3e53dee2e9
L <L@LAPTOP-6N6PPQII>
2022-04-05 17:17:01 +0800

    添加项目文件。

---
+++ 顺序表.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <cmath>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+const int N = 100;
+//线性表的动态建立
+typedef struct {
+	int *data;
+	int n;
+}SeqList;
+
+//线性表的静态建立
+typedef struct {
+	int data[N];
+	int n;
+} SqList;
+
+//
+int main() {
+
+	
+	return 0;
+}
